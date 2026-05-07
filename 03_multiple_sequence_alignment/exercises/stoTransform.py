ff5b3f90615c28c86f8aae0b24cd23cf8d02236b
cvmering <mering@imls.uzh.ch>
2026-05-07 17:24:11 +0200

    adjust to Windows-style encodings

--- 03_multiple_sequence_alignment/exercises/stoTransform.py
+++ 03_multiple_sequence_alignment/exercises/stoTransform.py
@@ -1,5 +1,6 @@
 from Bio import AlignIO
 import argparse
+import io
 import re
 
 parser = argparse.ArgumentParser(description='input and output files')
@@ -7,8 +8,22 @@ parser.add_argument('-i',action='store',dest='input', help='input file ')
 parser.add_argument('-o',action='store',dest='output', help='output file ')
 args = parser.parse_args()
 
-with open(args.input, "r", encoding="utf-8-sig") as handle:
-    align = AlignIO.read(handle, "stockholm")
+with open(args.input, "rb") as raw:
+    data = raw.read()
+
+# Try common encodings for Windows/hmmalign output
+for enc in ("utf-8-sig", "utf-16", "latin-1"):
+    try:
+        text = data.decode(enc)
+        if "# STOCKHOLM" in text[:100]:
+            break
+    except UnicodeDecodeError:
+        continue
+else:
+    raise ValueError("Could not decode input file")
+
+handle = io.StringIO(text)
+align = AlignIO.read(handle, "stockholm")
 
 align_dict=dict()
 
